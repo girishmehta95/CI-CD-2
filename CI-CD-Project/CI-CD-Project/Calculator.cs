87dc458965f82ff68656820bd62bf1f96a090413
girishmehta95 <girishmehta1995@gmail.com>
2025-04-16 11:07:24 +0530

    Initial commit

---
+++ CI-CD-Project/CI-CD-Project/Calculator.cs
@@ -0,0 +1,10 @@
+﻿namespace CI_CD_Project
+{
+    public static class Calculator
+    {
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
---
+++ CI-CD-Project/CalculatorTests/AddTests.cs
@@ -0,0 +1,16 @@
+﻿using CI_CD_Project;
+using NUnit.Framework;
+
+namespace CalculatorTests
+{
+    [TestFixture]
+    public class AddTests
+    {
+        [Test]
+        public void Add()
+        {
+            var result = Calculator.Add(10, 20);
+            Assert.Equals(30, result);
+        }
+    }
+}
